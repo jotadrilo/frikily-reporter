240c5e995d7cb03c4f291eade2a7fe377cec0ae6
Joseda Rios <jdrios@vmware.com>
2022-02-12 18:36:14 +0100

    Fix scripts

--- compress.sh
+++ compress.sh
@@ -4,6 +4,6 @@ set -e
 
 cd lambda-frikily >/dev/null
 echo "Compressing lambda..."
-rm ../lambda-frikily.zip
+rm -f ../lambda-frikily.zip
 zip -q -r ../lambda-frikily.zip *
 cd - >/dev/null
--- upload.sh
+++ upload.sh
@@ -4,7 +4,11 @@ set -e
 
 export AWS_REGION=eu-west-2
 
+./compress.sh
+
 echo "Uploading lambda..."
 aws lambda update-function-code \
     --function-name lambda-frikily \
     --zip-file fileb://lambda-frikily.zip
+
+rm -f ./lambda-frikily.zip
